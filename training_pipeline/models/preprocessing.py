1b72850dcf381e6d1c77ef4be645d455d5226ba6
Chansung Park <deep.diver.csp@gmail.com>
2022-09-03 17:25:17 +0000

    update preprocessing module

--- training_pipeline/models/preprocessing.py
+++ training_pipeline/models/preprocessing.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
-from tensorflow.keras.applications import resnet50
+from tensorflow.keras.applications import mobilenet
 
 _IMAGE_KEY = "image"
 _LABEL_KEY = "label"
-
+_IMAGE_SIZE = (128, 128)
 
 def _transformed_name(key: str) -> str:
     return key + "_xf"
-
 
 def preprocessing_fn(inputs):
     """tf.transform's callback function for preprocessing inputs.
@@ -23,10 +22,17 @@ def preprocessing_fn(inputs):
         inputs[_IMAGE_KEY],
         fn_output_signature=(tf.uint8),
     )
-    image_features = tf.image.resize(image_features, [128, 128])
-    image_features = resnet50.preprocess_input(image_features)
+    image_features = tf.image.resize(image_features, _IMAGE_SIZE)
+    image_features = mobilenet.preprocess_input(image_features)
+
+    label_features = tf.map_fn(
+        lambda x: tf.io.decode_png(x[0], channels=1),
+        inputs[_LABEL_KEY],
+        fn_output_signature=(tf.uint8),
+    )
+    label_features = tf.image.resize(label_features, _IMAGE_SIZE)
 
     outputs[_transformed_name(_IMAGE_KEY)] = image_features
-    outputs[_transformed_name(_LABEL_KEY)] = inputs[_LABEL_KEY]
+    outputs[_transformed_name(_LABEL_KEY)] = label_features
 
     return outputs
