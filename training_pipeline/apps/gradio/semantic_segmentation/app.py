0d276f4b594856b5fcd8ab0bc947014d6e4d14c7
Sayak Paul <spsayakpaul@gmail.com>
2022-09-15 09:55:49 +0530

    add: standalone gradio app. (#22)
    * add: standalone gradio app.
    
    * Update training_pipeline/apps/gradio/semantic_segmentation/requirements.txt
    
    Co-authored-by: Chansung Park <deep.diver.csp@gmail.com>
    
    * chore: address pr feedback.
    
    Co-authored-by: Chansung Park <deep.diver.csp@gmail.com>

--- training_pipeline/apps/gradio/semantic_segmentation/app.py
+++ training_pipeline/apps/gradio/semantic_segmentation/app.py
@@ -1,255 +1,91 @@
 import gradio as gr
-
-from matplotlib import gridspec
-import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 import tensorflow as tf
-from transformers import SegformerFeatureExtractor, TFSegformerForSemanticSegmentation
+from huggingface_hub import from_pretrained_keras
+from PIL import Image
 
-feature_extractor = SegformerFeatureExtractor.from_pretrained(
-    "nvidia/segformer-b5-finetuned-ade-640-640"
-)
-model = TFSegformerForSemanticSegmentation.from_pretrained(
-    "nvidia/segformer-b5-finetuned-ade-640-640"
-)
+MODEL_CKPT = "$MODEL_REPO_ID@$MODEL_VERSION"
+MODEL = from_pretrained_keras(MODEL_CKPT)
 
 
-def ade_palette():
-    """ADE20K palette that maps each class to RGB values."""
-    return [
-        [120, 120, 120],
-        [180, 120, 120],
-        [6, 230, 230],
-        [80, 50, 50],
-        [4, 200, 3],
-        [120, 120, 80],
-        [140, 140, 140],
-        [204, 5, 255],
-        [230, 230, 230],
-        [4, 250, 7],
-        [224, 5, 255],
-        [235, 255, 7],
-        [150, 5, 61],
-        [120, 120, 70],
-        [8, 255, 51],
-        [255, 6, 82],
-        [143, 255, 140],
-        [204, 255, 4],
-        [255, 51, 7],
-        [204, 70, 3],
-        [0, 102, 200],
-        [61, 230, 250],
-        [255, 6, 51],
-        [11, 102, 255],
-        [255, 7, 71],
-        [255, 9, 224],
-        [9, 7, 230],
-        [220, 220, 220],
-        [255, 9, 92],
-        [112, 9, 255],
-        [8, 255, 214],
-        [7, 255, 224],
-        [255, 184, 6],
-        [10, 255, 71],
-        [255, 41, 10],
-        [7, 255, 255],
-        [224, 255, 8],
-        [102, 8, 255],
-        [255, 61, 6],
-        [255, 194, 7],
-        [255, 122, 8],
-        [0, 255, 20],
-        [255, 8, 41],
-        [255, 5, 153],
-        [6, 51, 255],
-        [235, 12, 255],
-        [160, 150, 20],
-        [0, 163, 255],
-        [140, 140, 140],
-        [250, 10, 15],
-        [20, 255, 0],
-        [31, 255, 0],
-        [255, 31, 0],
-        [255, 224, 0],
-        [153, 255, 0],
-        [0, 0, 255],
-        [255, 71, 0],
-        [0, 235, 255],
-        [0, 173, 255],
-        [31, 0, 255],
-        [11, 200, 200],
-        [255, 82, 0],
-        [0, 255, 245],
-        [0, 61, 255],
-        [0, 255, 112],
-        [0, 255, 133],
-        [255, 0, 0],
-        [255, 163, 0],
-        [255, 102, 0],
-        [194, 255, 0],
-        [0, 143, 255],
-        [51, 255, 0],
-        [0, 82, 255],
-        [0, 255, 41],
-        [0, 255, 173],
-        [10, 0, 255],
-        [173, 255, 0],
-        [0, 255, 153],
-        [255, 92, 0],
-        [255, 0, 255],
-        [255, 0, 245],
-        [255, 0, 102],
-        [255, 173, 0],
-        [255, 0, 20],
-        [255, 184, 184],
-        [0, 31, 255],
-        [0, 255, 61],
-        [0, 71, 255],
-        [255, 0, 204],
-        [0, 255, 194],
-        [0, 255, 82],
-        [0, 10, 255],
-        [0, 112, 255],
-        [51, 0, 255],
-        [0, 194, 255],
-        [0, 122, 255],
-        [0, 255, 163],
-        [255, 153, 0],
-        [0, 255, 10],
-        [255, 112, 0],
-        [143, 255, 0],
-        [82, 0, 255],
-        [163, 255, 0],
-        [255, 235, 0],
-        [8, 184, 170],
-        [133, 0, 255],
-        [0, 255, 92],
-        [184, 0, 255],
-        [255, 0, 31],
-        [0, 184, 255],
-        [0, 214, 255],
-        [255, 0, 112],
-        [92, 255, 0],
-        [0, 224, 255],
-        [112, 224, 255],
-        [70, 184, 160],
-        [163, 0, 255],
-        [153, 0, 255],
-        [71, 255, 0],
-        [255, 0, 163],
-        [255, 204, 0],
-        [255, 0, 143],
-        [0, 255, 235],
-        [133, 255, 0],
-        [255, 0, 235],
-        [245, 0, 255],
-        [255, 0, 122],
-        [255, 245, 0],
-        [10, 190, 212],
-        [214, 255, 0],
-        [0, 204, 255],
-        [20, 0, 255],
-        [255, 255, 0],
-        [0, 153, 255],
-        [0, 41, 255],
-        [0, 255, 204],
-        [41, 0, 255],
-        [41, 255, 0],
-        [173, 0, 255],
-        [0, 245, 255],
-        [71, 0, 255],
-        [122, 0, 255],
-        [0, 255, 184],
-        [0, 92, 255],
-        [184, 255, 0],
-        [0, 133, 255],
-        [255, 214, 0],
-        [25, 194, 194],
-        [102, 255, 0],
-        [92, 0, 255],
-    ]
+RESOLTUION = 128
 
-
-labels_list = []
-
-with open(r"labels.txt", "r") as fp:
+ADE_PALETTE = []
+with open(r"./palette.txt", "r") as fp:
     for line in fp:
-        labels_list.append(line[:-1])
-
-colormap = np.asarray(ade_palette())
-
-
-def label_to_color_image(label):
-    if label.ndim != 2:
-        raise ValueError("Expect 2-D input label")
-
-    if np.max(label) >= len(colormap):
-        raise ValueError("label value too large.")
-
-    return colormap[label]
+        tmp_list = list(map(int, line[:-1].strip("][").split(", ")))
+        ADE_PALETTE.append(tmp_list)
 
 
-def draw_plot(pred_img, seg):
-    fig = plt.figure(figsize=(20, 15))
+def preprocess_input(image: Image) -> tf.Tensor:
+    image = np.array(image)
+    image = tf.convert_to_tensor(image)
 
-    grid_spec = gridspec.GridSpec(1, 2, width_ratios=[6, 1])
+    image = tf.image.resize(image, (RESOLTUION, RESOLTUION))
+    image = image / 255
 
-    plt.subplot(grid_spec[0])
-    plt.imshow(pred_img)
-    plt.axis("off")
-
-    LABEL_NAMES = np.asarray(labels_list)
-    FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
-    FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
-
-    unique_labels = np.unique(seg.numpy().astype("uint8"))
-    ax = plt.subplot(grid_spec[1])
-    plt.imshow(FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation="nearest")
-    ax.yaxis.tick_right()
-    plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
-    plt.xticks([], [])
-    ax.tick_params(width=0.0, labelsize=25)
-    return fig
+    return tf.expand_dims(image, 0)
 
 
-def sepia(input_img):
-    input_img = Image.fromarray(input_img)
+# The below utility get_seg_overlay() are from:
+# https://github.com/deep-diver/semantic-segmentation-ml-pipeline/blob/main/notebooks/inference_from_SavedModel.ipynb
 
-    inputs = feature_extractor(images=input_img, return_tensors="tf")
-    outputs = model(**inputs)
-    logits = outputs.logits
 
-    logits = tf.transpose(logits, [0, 2, 3, 1])
-    logits = tf.image.resize(
-        logits, input_img.size[::-1]
-    )  # We reverse the shape of `image` because `image.size` returns width and height.
-    seg = tf.math.argmax(logits, axis=-1)[0]
-
+def get_seg_overlay(image, seg):
     color_seg = np.zeros(
         (seg.shape[0], seg.shape[1], 3), dtype=np.uint8
     )  # height, width, 3
+    palette = np.array(ADE_PALETTE)
 
-    for label, color in enumerate(colormap):
+    for label, color in enumerate(palette):
         color_seg[seg == label, :] = color
 
-    # Convert to BGR
-    color_seg = color_seg[..., ::-1]
-
     # Show image + mask
-    pred_img = np.array(input_img) * 0.5 + color_seg * 0.5
-    pred_img = pred_img.astype(np.uint8)
+    img = np.array(image) * 0.5 + color_seg * 0.5
 
-    fig = draw_plot(pred_img, seg)
-    return fig
+    img *= 255
+    img = np.clip(img, 0, 255)
+    img = img.astype(np.uint8)
+    return img
 
+
+def run_model(image: Image) -> tf.Tensor:
+    preprocessed_image = preprocess_input(image)
+    prediction = MODEL.predict(preprocessed_image)
+
+    seg_mask = tf.math.argmax(prediction, -1)
+    seg_mask = tf.squeeze(seg_mask)
+    return seg_mask
+
+
+def get_predictions(image: Image):
+    predicted_segmentation_mask = run_model(image)
+    preprocessed_image = preprocess_input(image)
+    preprocessed_image = tf.squeeze(preprocessed_image, 0)
+
+    pred_img = get_seg_overlay(
+        preprocessed_image.numpy(), predicted_segmentation_mask.numpy()
+    )
+    return Image.fromarray(pred_img)
+
+
+title = "Simple demo for a semantic segmentation model trained on the Sidewalks dataset."
+
+description = """
+
+Note that the outputs obtained in this demo won't be state-of-the-art. The underlying project has a different objective focusing more on the ops side of
+deploying a semantic segmentation model. For more details, check out the repository: https://github.com/deep-diver/semantic-segmentation-ml-pipeline/.
+
+"""
 
 demo = gr.Interface(
-    sepia,
-    gr.Image(shape=(200, 200)),
-    outputs=["plot"],
-    #                    examples=["ADE_val_00000001.jpeg"],
+    get_predictions,
+    gr.inputs.Image(type="pil"),
+    "pil",
     allow_flagging="never",
+    title=title,
+    description=description,
+    examples=[["test-image.jpg"]],
 )
 
 demo.launch()
