a8aeae62bc39111c4b089a6231b2467dd1a989ba
Chansung Park <deep.diver.csp@gmail.com>
2022-09-15 13:31:46 +0000

    .

--- training_pipeline/models/model.py
+++ training_pipeline/models/model.py
@@ -72,17 +72,19 @@ def _model_exporter(model: tf.keras.Model):
 """
 
 
-def _parse_tfr(proto):
+def parse_tfr(proto):
     feature_description = {
-        "image": tf.io.FixedLenFeature([], tf.string),
-        "label": tf.io.FixedLenFeature([], tf.string),
+        "image": tf.io.VarLenFeature(tf.float32),
+        "image_shape": tf.io.VarLenFeature(tf.int64),
+        "label": tf.io.VarLenFeature(tf.float32),
+        "label_shape": tf.io.VarLenFeature(tf.int64),
     }
     rec = tf.io.parse_single_example(proto, feature_description)
-
-    image = tf.io.parse_tensor(rec["image"], tf.float32)
-    label = tf.io.parse_tensor(rec["label"], tf.float32)
-
-    return {"pixel_values": image, "labels": label}
+    image_shape = tf.sparse.to_dense(rec["image_shape"])
+    image = tf.reshape(tf.sparse.to_dense(rec["image"]), image_shape)
+    label_shape = tf.sparse.to_dense(rec["label_shape"])
+    label = tf.reshape(tf.sparse.to_dense(rec["label"]), label_shape)
+    return {"pixel_values": image, "label": label}
 
 
 def _preprocess(example_batch):
