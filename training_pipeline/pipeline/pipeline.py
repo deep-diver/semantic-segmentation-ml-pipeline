6c63269310f364cb900e137e88746e406d5b61e8
Chansung Park <deep.diver.csp@gmail.com>
2022-11-05 15:40:12 +0000

    set cache back to True

--- training_pipeline/pipeline/pipeline.py
+++ training_pipeline/pipeline/pipeline.py
@@ -125,6 +125,6 @@ def create_pipeline(
         pipeline_name=pipeline_name,
         pipeline_root=pipeline_root,
         components=components,
-        enable_cache=False,
+        enable_cache=True,
         metadata_connection_config=metadata_connection_config,
     )
