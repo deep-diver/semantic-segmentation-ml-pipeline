f22985d24defa003d43120566b254680bd59c3d2
Chansung Park <deep.diver.csp@gmail.com>
2022-12-01 11:28:16 +0900

    Update signatures.py

--- training_pipeline/models/signatures.py
+++ training_pipeline/models/signatures.py
@@ -132,7 +132,7 @@ def tf_examples_serving_signature(model, tf_transform_output):
         serialized_tf_example: tf.Tensor,
     ) -> Dict[str, tf.Tensor]:
         feature_spec = tf_transform_output.raw_feature_spec()
-        features = tf.io.parse_example(serialized_tf_example, raw_feature_spec)
+        transformed_features = tf.io.parse_example(serialized_tf_example, feature_spec)
 
         outputs = model(transformed_features)
 
